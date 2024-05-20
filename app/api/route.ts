5661f694418c5f989afab259fdcd37fc9e45b96e
Sambit Mondal <sambitmondal2005@gmail.com>
2024-05-21 01:21:33 +0530

    Fixes #35

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,42 +1,28 @@
 import { NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
 
-
-
-
 export async function POST(request: Request) {
-  let browser;
   try {
     const { url } = await request.json();
 
     const urlRegex = /^(http|https):\/\/[^ "]+$/;
 
     if (!urlRegex.test(url)) {
-        return NextResponse.json({urlerror:"invalid url"});
+      return NextResponse.json({ urlerror: "invalid url" });
     }
 
-    if (process.env.API) {
-      // If the API URL is present, connect to the existing browser instance
-      console.log("api tested");
-      browser = await puppeteer.connect({ browserWSEndpoint: process.env.API });
-  } else {
-      // If the API URL is not present, launch a new browser instance
-      browser = await puppeteer.launch();
-  }
+    const browser = process.env.API
+      ? await puppeteer.connect({ browserWSEndpoint: process.env.API })
+      : await puppeteer.launch();
+
     const page = await browser.newPage();
-    console.log(url);
     await page.goto(url);
-    const extractedText = await page.$eval('*', (el: any) => el.innerText);
-    console.log(extractedText);
-    if (browser) {
-      await browser.close();
-    }
+    const extractedText = await page.evaluate(() => document.body.innerText);
+
+    await browser.close();
 
     return NextResponse.json({ extractedText });
   } catch (error) {
-    if (browser) {
-      await browser.close();
-    }
     console.error(error);
     return NextResponse.json({ error });
   }
