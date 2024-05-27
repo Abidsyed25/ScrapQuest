56a69ea0865acc0f0bc9754116fe02e6814321ed
Soumyajit Roy <roysoumyajit36@gmail.com>
2024-05-27 19:59:26 +0530

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,31 +1,29 @@
-"use client";
 import Link from "next/link";
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 import {
-    NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-  } from "@/components/ui/navigation-menu"  
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
 import Social from "./Social";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { MenuIcon } from "lucide-react";
-import { useState } from "react";
 
-export function Nav() 
-{
-    return (
-        <Navbar fluid rounded className="bg-transparent shadow-md max-w-screen-2xl m-auto">
-
-            <NavbarBrand as={Link} href="/" className="pl-9">
-                <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">ScrapQuest</span>
-            </NavbarBrand>         
-            <Social/>
-
-        </Navbar>
-    );
+export function Nav() {
+  return (
+    <Navbar
+      fluid
+      rounded
+      className="bg-gradient-to-r from-blue-500 shadow-md max-w-screen-2xl m-auto"
+    >
+      <NavbarBrand as={Link} href="/" className="pl-9">
+        <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+          ScrapQuest
+        </span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <Social />
+      </NavbarCollapse>
+    </Navbar>
+  );
 }
