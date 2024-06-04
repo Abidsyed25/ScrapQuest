6819f435f81242d2f06fdfcfdf901dda7b67f0fc
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-06-04 10:34:24 +0530

    completed footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import { Nav } from "@/components/Nav";
 import Image from "next/image";
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="bg-gray-950" style={{ height: "100vh" }}>
       <Nav />
       <Hero />
+      <Footer/>
     </div>
   );
 }
