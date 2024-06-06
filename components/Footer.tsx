a649d22a355a54305eb68b3104c2061af6b23dbe
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-06-06 19:51:31 +0530

    changed color to nav

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <div className="fixed bottom-0 left-0 w-screen opacity-45 bg-[#1a1a1a] py-3 tracking-wider text-center text-sm">
+        <div className="fixed bg-gradient-to-r from-blue-500  shadow-md bottom-0 left-0 w-screen text-white py-3 tracking-wider text-center text-sm">
             <h1>Copyright © 2024, ScrapQuest. All Rights Reserved.</h1>
         </div>
     )
