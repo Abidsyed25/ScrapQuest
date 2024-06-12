07736184cb49a2ee88f120c79b894d8363050539
Abid <116893970+Abidsyed25@users.noreply.github.com>
2024-06-12 13:41:48 +0530

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -41,3 +41,7 @@ export async function POST(request: Request) {
     return NextResponse.json({ error });
   }
 }
+
+export async function GET(request: Request) {
+  return NextResponse.json({a:"get request working"});
+}
