1976931497bb43a7096abd3a441d4de62d7aa459
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-06-17 22:11:14 +0530

    Footer Updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,69 +1,26 @@
-// export default function Footer(){
-//     return (
-//         <div className="fixed bg-gradient-to-r from-blue-500  shadow-md bottom-0 left-0 w-screen text-white py-3 tracking-wider text-center text-sm">
-//             <h1>Copyright © 2024, ScrapQuest. All Rights Reserved.</h1>
-//         </div> 
-//     )
-// }
-
-// export default function Footer() {
-//     return (
-//         <div className="fixed bg-gradient-to-r from-blue-500 shadow-md bottom-0 left-0 w-screen text-white py-3 tracking-wider text-center text-sm">
-           
-//             <div className="flex justify-center space-x-6 mt-2">
-//                 <a href="/contact-us" className="hover:underline">Contact Us</a>
-//                 <a href="/about-us" className="hover:underline">About Us</a>
-//                 <div className="flex space-x-4">
-//                     <a href="https://github.com/Abidsyed25/ScrapQuest" target="_blank" rel="noopener noreferrer" className="text-white  font-medium  text-sm px-5 py-2.5 text-center inline-flex  ">
-//                     <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-//                         <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd" />
-//                     </svg>Star on Github</a>
-                   
-//                 </div>
-
-//             </div>
-//             <h1>Copyright © 2024, ScrapQuest. All Rights Reserved.</h1>
-//         </div>
-//     )
-// }
-
-// export default function Footer() {
-//     return (
-//         <div className="fixed bg-gradient-to-r from-blue-500 shadow-md bottom-0 left-0 w-screen text-white py-3 tracking-wider text-center text-sm">
-//             <div className="flex flex-col items-center space-y-2">
-//                 <div className="flex justify-center space-x-6 mt-2">
-//                     <a href="/contact-us" className="hover:underline">Contact Us</a>
-//                     <a href="/about-us" className="hover:underline">About Us</a>
-//                     <a href="https://github.com/Abidsyed25/ScrapQuest" target="_blank" rel="noopener noreferrer" className="text-white font-medium text-sm px-5 py-2.5 text-center inline-flex items-center hover:underline">
-//                         <svg className="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-//                             <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd" />
-//                         </svg>
-//                         Star on Github
-//                     </a>
-//                 </div>
-//                 <h1 className="mt-2">Copyright © 2024, ScrapQuest. All Rights Reserved.</h1>
-//             </div>
-//         </div>
-//     );
-// }
-
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
-    return (
-        <div className="fixed bg-gradient-to-r from-blue-500 shadow-md bottom-0 left-0 w-screen text-white py-3 tracking-wider text-center text-sm">
-            <div className="flex flex-col items-center">
-                <div className="flex justify-center space-x-6">
-                    <a href="/contact" className=" hover:text-blue-400">Contact Us</a>
-                    <a href="/about" className="hover:text-blue-400">About Us</a>
-                </div>
-                <a href="https://github.com/Abidsyed25/ScrapQuest" target="_blank" rel="noopener noreferrer" className="text-white font-medium text-sm px-5 py-2.5 text-center inline-flex items-center mt-2 hover:text-blue-400">
-                    <svg className="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd" />
-                    </svg>
-                    Star on Github
-                </a>
-                <h1 className="mt-2">Copyright © 2024, ScrapQuest. All Rights Reserved.</h1>
-            </div>
+  return (
+    <div className="fixed bg-gradient-to-r from-blue-500 shadow-md bottom-0 left-0 w-screen text-white py-3 tracking-wider text-center text-sm">
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center space-x-6 items-center">
+          <Link href="/" className="hover:text-blue-400">Home</Link>
+          <Link href="/about" className="hover:text-blue-400">About Us</Link>
+          <Link href="/contact" className="hover:text-blue-400">Contact</Link>
+          <Link
+            href="https://github.com/Abidsyed25/ScrapQuest"
+            rel="noopener noreferrer"
+            className="text-white font-medium text-sm flex items-center hover:text-blue-400"
+          >
+            <FaGithub className="w-4 h-4 mr-2" /> Star on Github
+          </Link>
+          <Link href="/faq" className="hover:text-blue-400">FAQ</Link>
+          <Link href="/login" className="hover:text-blue-400">Begin Your Journey</Link>
         </div>
-    );
+        <h1 className="mt-2">Copyright © 2024, ScrapQuest. All Rights Reserved.</h1>
+      </div>
+    </div>
+  );
 }
