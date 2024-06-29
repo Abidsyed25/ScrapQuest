ee4c446b1973b3e7a150bccccc61877acd80055f
Sailaja Adapa <adapasailaja17@gmail.com>
2024-06-29 17:26:16 +0530

    Added Icons to Footer

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FaGithub } from "react-icons/fa";
+import { FaHome, FaInfoCircle, FaEnvelope, FaGithub, FaQuestionCircle, FaSignInAlt } from "react-icons/fa";
 import { Navbar } from "flowbite-react";
 
 export function Nav() {
@@ -15,38 +15,28 @@ export function Nav() {
         <Navbar.Toggle className="text-white " />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link href="/" className="text-white text-lg hover:underline ">
+        <Navbar.Link href="/" className="text-white text-lg hover:underline flex items-center">
+          <FaHome className="mr-2" />
           Home
         </Navbar.Link>
-        <Navbar.Link
-          href="/about"
-          className="text-white text-lg hover:underline "
-        >
+        <Navbar.Link href="/about" className="text-white text-lg hover:underline flex items-center">
+          <FaInfoCircle className="mr-2" />
           About us
         </Navbar.Link>
-        <Navbar.Link
-          href="/contact"
-          className="text-white text-lg hover:underline "
-        >
+        <Navbar.Link href="/contact" className="text-white text-lg hover:underline flex items-center">
+          <FaEnvelope className="mr-2" />
           Contact
         </Navbar.Link>
-        <Navbar.Link
-          href="https://github.com/Abidsyed25/ScrapQuest"
-          className="text-white text-lg hover:underline flex items-center "
-        >
+        <Navbar.Link href="https://github.com/Abidsyed25/ScrapQuest" className="text-white text-lg hover:underline flex items-center">
           <FaGithub className="mr-2" />
-          <p> Star on Github</p>
+          Star on Github
         </Navbar.Link>
-        <Navbar.Link
-          href="/faq"
-          className="text-white text-lg hover:underline "
-        >
+        <Navbar.Link href="/faq" className="text-white text-lg hover:underline flex items-center">
+          <FaQuestionCircle className="mr-2" />
           FAQ
         </Navbar.Link>
-        <Navbar.Link
-          href="/login"
-          className="text-white text-lg hover:underline "
-        >
+        <Navbar.Link href="/login" className="text-white text-lg hover:underline flex items-center">
+          <FaSignInAlt className="mr-2" />
           Begin Your Journey
         </Navbar.Link>
       </Navbar.Collapse>
