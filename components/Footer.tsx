0b28158f9820702fb245fdf8ee2a2018785e3736
AMS <111883236+AMS003010@users.noreply.github.com>
2024-07-09 14:17:08 +0530

    fixed footer issue and also footer responsiveness issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,55 +17,59 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function Footer() {
   return (
-    <div className="fixed bg-gradient-to-r from-blue-500 shadow-md bottom-0 left-0 w-screen text-white py-3 tracking-wider text-center text-sm">
+    <div className="bg-gradient-to-r from-blue-500 shadow-md w-screen text-white py-3 tracking-wider text-center text-sm max-[768px]:text-xs">
       <div className="flex flex-col items-center">
-        <div className="flex justify-center space-x-6 items-center">
-          <Link href="/" legacyBehavior>
-            <a className="hover:text-blue-400 flex items-center">
-              <FaHome className="w-4 h-4 mr-2" />
-              Home
-            </a>
-          </Link>
-          <Link href="/about" legacyBehavior>
-            <a className="hover:text-blue-400 flex items-center">
-              <FaInfoCircle className="w-4 h-4 mr-2" />
-              About Us
-            </a>
-          </Link>
-          <Link href="/contact" legacyBehavior>
-            <a className="hover:text-blue-400 flex items-center">
-              <FaEnvelope className="w-4 h-4 mr-2" />
-              Contact
-            </a>
-          </Link>
-          <Link
-            href="https://github.com/Abidsyed25/ScrapQuest"
-            rel="noopener noreferrer"
-            legacyBehavior
-          >
-            <a className="text-white font-medium text-sm flex items-center hover:text-blue-400">
-              <FaGithub className="w-4 h-4 mr-2" />
-              Star on Github
-            </a>
-          </Link>
-          <Link href="/faq" legacyBehavior>
-            <a className="hover:text-blue-400 flex items-center">
-              <FaQuestionCircle className="w-4 h-4 mr-2" />
-              FAQ
-            </a>
-          </Link>
-          <Link href="/termsconditions" legacyBehavior>
-            <a className="hover:text-blue-400 flex items-center">
-              <FaPenSquare className="w-4 h-4 mr-2" />
-              Terms & Conditions
-            </a>
-          </Link>
-          <Link href="/login" legacyBehavior>
-            <a className="hover:text-blue-400 flex items-center">
-              <FaSignInAlt className="w-4 h-4 mr-2" />
-              Begin Your Journey
-            </a>
-          </Link>
+        <div className="flex max-[768px]:flex-col justify-between gap-4 items-center">
+          <div className="flex gap-4">
+            <Link href="/" legacyBehavior>
+              <a className="hover:text-blue-400 flex items-center">
+                <FaHome className="w-4 h-4 mr-1" />
+                Home
+              </a>
+            </Link>
+            <Link href="/about" legacyBehavior>
+              <a className="hover:text-blue-400 flex items-center">
+                <FaInfoCircle className="w-4 h-4 mr-1" />
+                About Us
+              </a>
+            </Link>
+            <Link href="/contact" legacyBehavior>
+              <a className="hover:text-blue-400 flex items-center">
+                <FaEnvelope className="w-4 h-4 mr-1" />
+                Contact
+              </a>
+            </Link>
+            <Link
+              href="https://github.com/Abidsyed25/ScrapQuest"
+              rel="noopener noreferrer"
+              legacyBehavior
+            >
+              <a className="text-white font-medium text-sm flex items-center hover:text-blue-400">
+                <FaGithub className="w-4 h-4 mr-1" />
+                Star on Github
+              </a>
+            </Link>
+          </div>
+          <div className="flex gap-4">
+            <Link href="/faq" legacyBehavior>
+              <a className="hover:text-blue-400 flex items-center">
+                <FaQuestionCircle className="w-4 h-4 mr-1" />
+                FAQ
+              </a>
+            </Link>
+            <Link href="/termsconditions" legacyBehavior>
+              <a className="hover:text-blue-400 flex items-center">
+                <FaPenSquare className="w-4 h-4 mr-1" />
+                Terms & Conditions
+              </a>
+            </Link>
+            <Link href="/login" legacyBehavior>
+                <a className="hover:text-blue-400 flex items-center">
+                  <FaSignInAlt className="w-4 h-4 mr-2" />
+                  Begin Your Journey
+                </a>
+            </Link>
+          </div>
         </div>
         <div className="mt-4">
           <p className="social-text" style={{ color: "turquoise" }}>
