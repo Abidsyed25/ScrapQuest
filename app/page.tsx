5db9075b7547b0b8520f010bb47d60fce0c2df4f
Sayyad Aslam <sayyadaslam2020@gmail.com>
2024-07-28 14:48:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import { Nav } from "@/components/Nav";
 import Image from "next/image";
+import CustomScrollbar from "@/components/CustomScrollbar";
 
 export default function Home() {
   return (
     <div className="bg-gray-950" style={{ height: "100vh" }}>
       <Nav />
+      <CustomScrollbar />
       <Hero />
       <Footer/>
     </div>
