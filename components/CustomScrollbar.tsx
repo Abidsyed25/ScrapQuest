0d5ac51cba7320df95b98551de70a03ef5b63843
Sayyad Aslam <sayyadaslam2020@gmail.com>
2024-07-28 14:47:58 +0530

    Added CustomScrollbar

---
+++ components/CustomScrollbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CustomScrollbar.module.css';  
+
+const CustomScrollbar: React.FC = () => {
+    return (
+        <div className={styles.customScrollbar} style={{ height: '200px' }}>
+            
+        </div>
+    );
+};
+
+export default CustomScrollbar;
